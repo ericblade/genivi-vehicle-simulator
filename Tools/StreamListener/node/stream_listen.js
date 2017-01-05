e78b4a348ae7bb3f481b004b111aec278016bcd8
Eric Blade <eric.blade@lge.com>
2017-01-05 14:19:21 -0500

    node stream_listen changes
    To more accurately simulate a real CAN bus, re-broadcast changed data
    at 50Hz (ish, depending on node.js latency) to all connections

--- Tools/StreamListener/node/stream_listen.js
+++ Tools/StreamListener/node/stream_listen.js
@@ -1,10 +1,58 @@
 const net = require('net');
+let connections = [];
+let timerInterval = null;
+let currentData = {};
+let lastSentData = {};
+
+const sendChangedData = () => {
+    let outStr = '';
+    Object.keys(currentData).forEach((k) => {
+        if (lastSentData[k] !== currentData[k]) {
+            outStr += `${k}, ${currentData[k]}\r\n`;
+            lastSentData[k] = currentData[k];
+        }
+    });
+    connections.forEach((c) => {
+        if (!c.destroyed)
+            c.write(outStr);
+    });
+};
+
+// data from genivi-simulator is in form
+// FieldName, data, number.
+// Last number appears to be a timestamp in number of seconds since start, probably for data
+// gathering purposes.
+const parseIncomingData = (data) => {
+    const arrayOfLines = data.match(/[^\r\n]+/g);
+    arrayOfLines.forEach((line) => {
+        const [fieldName, fieldData, fieldTimeStamp] = line.split(',');
+        if (currentData[fieldName] !== fieldData) { // only console log changes in data
+            console.log(`${fieldName}, ${fieldData}, ${fieldTimeStamp}`);
+        }
+        currentData[fieldName] = fieldData;
+    });
+};
+
+const lostConnection = (c) => {
+    connections = connections.filter((connection) => connection !== c);
+    console.log(`**** lost connection numConnections=${connections.length}`);
+    if (connections.length === 0) {
+        clearInterval(timerInterval);
+        timerInterval = null;
+    }
+}
 
 const server = net.createServer((c) => {
-    console.log('**** connected!!');
+    connections.push(c);
+    console.log(`**** connected!! numConnections=${connections.length}`);
+    if (!timerInterval) {
+        timerInterval = setInterval(sendChangedData, 20);
+    }
     c.on('data', (data) => {
-        console.log(data.toString('utf8'));
+        parseIncomingData(data.toString('utf8'));
     });
+    c.on('close', () => lostConnection(c));
+    c.on('error', () => {}); // dummy handler, so that socket receive errors don't crash. Assume fatals will destroy the socket. close should occur immediately after.
 });
 
 server.listen(9000, () => {
